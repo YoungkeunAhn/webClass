ddbbaf8c07006e1b652aa8f6f33663a27f568478
MYS <dkwlak10@gmail.com>
2021-12-30 11:00:18 +0900

    메뉴 collapse 기능 작업중

---
+++ CloneCoding/GangnamAcademy/script.js
@@ -0,0 +1,27 @@
+const menuList = [
+  "연간일정",
+  "아카데미소개",
+  "국비지원과정",
+  "정규교육과정",
+  "단과과정",
+  "커뮤니티",
+  "취업지원센터",
+  "기업위탁교육",
+  "상담센터",
+];
+
+// const openCollapseBox = () => {
+//   console.log("mouseover event");
+//   document.getElementById("collapseBox").style.height = "100%";
+// };
+
+// const closeCollapseBox = () => {
+//   console.log("mouseout event");
+//   document.getElementById("collapseBox").style.height = 0;
+// };
+
+// const menuBox = document.querySelector(".menu-list");
+// const collapseBox = document.querySelector(".collapse");
+
+// menuBox.addEventListener("mouseover", openCollapseBox);
+// collapseBox.addEventListener("mouseout", closeCollapseBox);
