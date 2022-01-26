b5efe66a518abf5358b38912db433425c2aeccd5
YoungkeunAhn <tosan2094@gmail.com>
2022-01-26 14:32:35 +0900

    home index 레이아웃

---
+++ CGV_PJ/home/initialData.js
@@ -0,0 +1,39 @@
+const menuItemList = ["멤버", "사진", "직캠", "라이브"]
+
+const mvList = [
+  {
+    title: "Forever",
+    image: "forever.jpg",
+    summary: `aespa 에스파 'Forever (약속)' MV`,
+    info: "",
+    url: "https://www.youtube.com/watch?v=wog1R1d4zls",
+  },
+  {
+    title: "Black mamaba",
+    image: "black_mamaba.jpg",
+    summary: `aespa 에스파 'Black Mamba' MV`,
+    info: "",
+    url: "https://www.youtube.com/watch?v=ZeerrnuLi5E",
+  },
+  {
+    title: "Next Level",
+    image: "next_level.jpg",
+    summary: ``,
+    info: "",
+    url: "https://www.youtube.com/watch?v=4TWR90KJl84",
+  },
+  {
+    title: "Savage",
+    image: "savage.jpg",
+    summary: `aespa 에스파 'Next Level' MV`,
+    info: "",
+    url: "https://www.youtube.com/watch?v=WPdWvnAAurg",
+  },
+  {
+    title: "Dreams come true",
+    image: "dreams_come_true.jpg",
+    summary: `[STATION] aespa 에스파 'Dreams Come True' MV`,
+    info: "",
+    url: "https://www.youtube.com/watch?v=H69tJmsgd9I",
+  },
+]
