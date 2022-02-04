abd47dd1be30966be5526e874db263129c95c7b9
YoungkeunAhn <tosan2094@gmail.com>
2022-02-04 15:55:38 +0900

    전체화면 완료

---
+++ CGV_PJ/home/events.js
@@ -0,0 +1,8 @@
+const mvFrame = document.getElementById("MVIframe")
+const mvSummary = document.getElementById("MVSummary")
+
+const onClickThumbnail = (url, summary) => {
+  // mvFrame.src = item.url
+  // mvSummary.innerHTML = item.summary
+  console.log(url, summary)
+}
--- CGV_PJ/home/initialData.js
+++ CGV_PJ/home/initialData.js
@@ -4,36 +4,41 @@ const mvList = [
   {
     title: "Forever",
     image: "forever.jpg",
-    summary: `aespa 에스파 'Forever (약속)' MV`,
-    info: "",
-    url: "https://www.youtube.com/watch?v=wog1R1d4zls",
+    summary: `‘글로벌 슈퍼 루키’ aespa, 싱글 ‘Forever (약속)’ 공개! 따뜻한 겨울 감성 리메이크 곡으로 색다른 매력 선사!`,
+    info: `2021. 02. 05. (금) 18:00 발매<br/>
+    지난 2000년 발매된 겨울 시즌 앨범 ‘Winter Vacation In SMTOWN.com’에 수록된 유영진의 ‘Forever (아이의 크리스마스 선물을 준비하는 아빠들에게)’를 새롭게 리메이크한 것으로, 풋풋하고 맑은 aespa의 음색이 돋보인다.`,
+    url: "https://www.youtube.com/embed/wog1R1d4zls",
   },
   {
     title: "Black mamaba",
-    image: "black_mamaba.jpg",
-    summary: `aespa 에스파 'Black Mamba' MV`,
-    info: "",
-    url: "https://www.youtube.com/watch?v=ZeerrnuLi5E",
+    image: "black_mamba.jpg",
+    summary: `SM 신인 걸그룹 aespa, 데뷔 싱글 ‘Black Mamba’ 공개! 파워풀한 댄스곡! 주문 외우는 듯한 훅으로 중독성 UP!`,
+    info: `2020. 11. 17. (화) 18:00 발매<br/>
+    ‘Black Mamba’는 시그니처 신스 사운드와 강렬한 베이스를 바탕으로 한 파워풀한 댄스곡으로 주문을 외우는 듯한 캐치한 훅이 돋보인다. 특히 가사에는 aespa와 아바타 ‘æ’의 연결을 방해하고 세상을 위협하는 존재가 ‘Black Mamba’라는 것을 알게 되면서 펼쳐지는 모험을 세계관 스토리로 담아낸 것이 특징이다.`,
+    url: "https://www.youtube.com/embed/ZeerrnuLi5E",
   },
   {
     title: "Next Level",
     image: "next_level.jpg",
-    summary: ``,
-    info: "",
-    url: "https://www.youtube.com/watch?v=4TWR90KJl84",
+    summary: `‘글로벌 슈퍼 루키’ aespa, 싱글 ‘Next Level’ 공개! 세계관 스토리 담은 힙합 댄스곡! 파워풀 매력 선사!`,
+    info: `2021. 05. 17. (월) 18:00 발매<br/>
+    ‘Next Level’은 영화 ‘분노의 질주: 홉스&쇼 (Fast & Furious: Hobbs & Shaw)’의 OST ‘Next Level’을 aespa만의 색깔로 리메이크, 그루비한 랩과 에너지 넘치는 베이스리프가 돋보이는 힙합 댄스곡으로 재탄생했으며, aespa 멤버들의 파워풀한 보이스와 버라이어티한 곡 진행이 돋보인다. 또한 가사에는 aespa와 아바타 ‘æ’의 ‘SYNK’를 방해하고 세상을 혼란에 빠뜨린 ‘Black Mamba’를 찾기 위해 ‘광야(KWANGYA)’로 떠나는 여정을 담은 세계관 스토리를 흥미롭게 담아냈다.`,
+    url: "https://www.youtube.com/embed/4TWR90KJl84",
   },
   {
     title: "Savage",
     image: "savage.jpg",
-    summary: `aespa 에스파 'Next Level' MV`,
-    info: "",
-    url: "https://www.youtube.com/watch?v=WPdWvnAAurg",
+    summary: `‘메타버스 걸그룹’ aespa, 첫 미니앨범 ‘Savage’ 발매! 또 한 번의 신드롬 예고! 독보적 세계관 스토리 이어간다!`,
+    info: `2021. 10. 05. (화) 18:00 발매<br/>
+    타이틀 곡 ‘Savage’는 강렬한 어택감의 드럼, 베이스를 중심으로 한 트랩 장르의 곡으로, 개성 있는 랩과 파워풀한 애드리브는 물론 중독성 있는 훅과 추임새가 돋보인다. 특히 이번 타이틀 곡 역시 aespa만의 세계관을 바탕으로 하여, aespa와 아바타 ‘æ’가 조력자 ‘Nævis’의 도움으로 광야(KWANGYA)로 나아가 ‘Black Mamba’와 맞서는 스토리를 흥미롭게 담아낸 가사가 인상적이다.`,
+    url: "https://www.youtube.com/embed/WPdWvnAAurg",
   },
   {
     title: "Dreams come true",
     image: "dreams_come_true.jpg",
-    summary: `[STATION] aespa 에스파 'Dreams Come True' MV`,
-    info: "",
-    url: "https://www.youtube.com/watch?v=H69tJmsgd9I",
+    summary: `에스파와 보아가 만났다! 재탄생한 ‘Dreams Come True’ 공개! 보아, 프로듀싱부터 안무+비주얼까지 리마스터링 프로젝트 적극 참여! 27일 발매 SMTOWN 겨울 앨범 선공개 곡!`,
+    info: `2021. 12. 20. (월) 18:00 발매<br/>
+    ‘Dreams Come True’는 ‘리마스터링 프로젝트’의 일환으로 선보이는 것으로, S.E.S.가 1998년 발표한 정규 2집 타이틀 곡을 에스파 버전으로 재해석, 원곡의 신비롭고 몽환적인 사운드에 에스파만의 영(Young)한 에너지와 힙합 바이브가 더해져 또 다른 매력을 느낄 수 있다.`,
+    url: "https://www.youtube.com/embed/H69tJmsgd9I",
   },
 ]
